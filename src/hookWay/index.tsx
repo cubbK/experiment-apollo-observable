22196031a0e03642b8c1f34911c8d43365d0400f
Dan Popa <danpopa1998@gmail.com>
2021-10-08 15:14:34 +0200

    group in folder

---
+++ src/hookWay/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
